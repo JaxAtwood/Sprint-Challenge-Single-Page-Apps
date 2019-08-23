82efb6a0e88b8bbf1217e041da1edb379158fb4c
Jackie Atwood <takalaj@verizon.net>
2019-08-23 15:01:03 -0400

    Message.....

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,19 @@
 import React from "react";
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
-}
+const LocationCard = props => {
+  const { name, type, dimension, residents } = props.location;
+
+  return (
+    <span className="location-card">
+      <h1>{name}</h1>
+      <p>{type}: {dimension}</p>
+      {residents.map(resident => (
+        <div key={resident} className="po-res">
+          {resident}
+        </div>
+      ))}
+    </span>
+  );
+};
+
+export default LocationCard;
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import LocationCard from "./LocationCard";
+import { Link } from "react-router-dom";
 
-export default function LocationsList() {}
+const LocationsList = () => {
+    const [locations, setLocations] = useState([]);
+  // TODO: Add useState to track data from useEffect
+
+  useEffect(() => {
+      const getLocations = () => {
+    // TODO: Add API Request here - must run in `useEffect`
+    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+        .get("https://rickandmortyapi.com/api/location/")
+        .then(res => {
+            console.log(res.data);
+            setLocations(res.data.results);
+        });
+  }
+
+  getLocations();
+ }, []);
+  
+  return (
+    <section className="location-list">
+      {locations.map(location => {
+        <Link to={`/location/${location.info}`}>
+            <LocationCard key={location.info} location={location} />
+        </Link>
+      })};
+    </section>
+    );
+}
+
+export default LocationsList;
